1cdb6f0bb69e326c9b8b28ba042ff8132149c8e5
ElendilTheTall <alistoffish@gmail.com>
2015-04-12 09:12:36 +0100

    Added CurrentWeather model and begin getting JSON

--- app/src/main/java/com/hyperglobal/stormy/MainActivity.java
+++ app/src/main/java/com/hyperglobal/stormy/MainActivity.java
@@ -16,12 +16,17 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
+
+    private CurrentWeather mCurrentWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,13 +57,16 @@ public class MainActivity extends ActionBarActivity {
                 @Override
                 public void onResponse(Response response) throws IOException {
                     try {
-                        Log.v(TAG, response.body().string());
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
                         if (response.isSuccessful()) {
-
+                            mCurrentWeather = getCurrentDetails(jsonData);
                         } else {
                             alertUserAboutError();
                         }
                     } catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    } catch (JSONException e){
                         Log.e(TAG, "Exception caught: ", e);
                     }
                 }
@@ -67,6 +75,13 @@ public class MainActivity extends ActionBarActivity {
             Toast.makeText(this,getString(R.string.network_unavailable_message),Toast.LENGTH_LONG).show();
         } // end of network availability check
 
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException {
+        JSONObject forecast = new JSONObject(jsonData);
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG,"From JSON: " + timezone);
+        return new CurrentWeather();
     }
 
     private boolean isNetWorkAvailable() {
